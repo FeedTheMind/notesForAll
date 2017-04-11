8064106581ac49121e2114c84683b4982da837f2
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-04-11 14:24:06 -0400

    Add more npm notes: install, update, etc.

--- npm/app.js
+++ npm/app.js
@@ -1,8 +1,9 @@
 var unsecuredPassword = 'password';
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt'); // require function allows us to access modules
+var colors = require('colors');
 
 bcrypt.genSalt(10, function(err, salt) {
   bcrypt.hash(unsecuredPassword, salt, function(err, hash) {
-    console.log(hash);  
+    console.log(hash.green);  
   });
 });
